97e67918cf6ac2d4d3c6c36705adfc27acd5e9f0
paweldymowski <pawel911@gmail.com>
2017-04-05 12:06:30 +0200

    square created

--- SnakeGame.pro
+++ SnakeGame.pro
@@ -23,7 +23,6 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 
-SOURCES += main.cpp\
-        mainwindow.cpp
+SOURCES += main.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  +=
--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect->setRect(0,0, 20, 20);
+
+    scene->addItem(rect);
+
+    QGraphicsView * view = new QGraphicsView(scene);
+
+    view->show();
+
+
+
 
     return a.exec();
 }
